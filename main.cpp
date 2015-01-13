ab03ca9d093f7b7c17996ec6c47d9ade5c8b9f08
EllieAnsell <ellie.ansell@live.co.uk>
2015-01-13 16:43:46 +0000

    made a triangle

--- main.cpp
+++ main.cpp
@@ -40,20 +40,36 @@ int main()
                    );
     glMatrixMode(GL_MODELVIEW);
     gluLookAt(2,2,2,0,0,0,0,1,0);
-    drawTriangle();
-    SDL_GL_SwapWindow(window);
-    SDL_Delay(10000);
+
+    bool quit=false;
+    SDL_Event event;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            switch( event.type)
+            {
+                case SDL_QUIT : quit=true; break;
+            }
+        }
+        glClear(GL_COLOR_BUFFER_BIT);
+        drawTriangle();
+        SDL_GL_SwapWindow(window);
+    } //end quit loop
 }
 
 void drawTriangle()
 {
+    static int rot=0; //static is a bit of globabl memory where it retains its value between calls
+
     glPushMatrix();
+        glRotated(++rot, 0,1,0);
         glBegin(GL_TRIANGLES);
-            glColor3f(1.0f, 0.0f,0.0f);
+            glColor3f(0.6f, 0.0f,0.0f);
             glVertex3f(0.0f, 1.0f,0.0f);
-            glColor3f(0.0f, 1.0f,0.0f);
+            glColor3f(0.2f, 0.0f,0.3f);
             glVertex3f(1.0,-1.0,0.0f);
-            glColor3f(-1.0f, -1.0f,0.0f);
+            glColor3f(1.0f, 1.0f,1.0f);
             glVertex3f(-1.0f, -1.0f,0.0f);
         glEnd();
     glPopMatrix();
